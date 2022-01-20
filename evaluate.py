d206f71d24739726f4cb27ad990549feb46489f0
Felix John <john@axantu.com>
2022-01-20 18:08:46 +0100

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -12,7 +12,7 @@ with open("tmp/output") as file:
         pred.append(int(split[1]))
 
 with open("tmp/evaluation", "a") as file:
-    file.write("Accuracy: " + str(f1_score(target, pred, average='weighted')) + "\n")
+    file.write("Accuracy: " + str(accuracy_score(target, pred) + "\n")
     file.write("F1-Score (Macro): " + str(f1_score(target, pred, average='macro')) + "\n")
     file.write("F1-Score (Micro): " + str(f1_score(target, pred, average='micro')) + "\n")
     file.write("Recall (Macro): " + str(recall_score(target, pred, average='macro')) + "\n")
@@ -20,7 +20,7 @@ with open("tmp/evaluation", "a") as file:
     file.write("Precision (Micro): " + str(precision_score(target, pred, average='macro')) + "\n")
     file.write("Precision (Micro): " + str(precision_score(target, pred, average='micro')) + "\n")
 
-print("Accuracy: ", f1_score(target, pred, average='weighted'))
+print("Accuracy: ", accuracy_score(target, pred))
 print("F1-Score (Macro): ", f1_score(target, pred, average='macro'))
 print("F1-Score (Micro): ", f1_score(target, pred, average='micro'))
 print("Recall (Macro): ", recall_score(target, pred, average='macro', zero_division=0))
